238c40c5c8e6ebc4726521da89f0c9962218f167
ZephirSD <siegfrieddallery@gmail.com>
2023-05-13 08:54:20 +0400

    Création de la Fil d'Ariane

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <>
       <Header/>
+      <div className="caseAriane">
+        <div className="filAriane">
+          Consoles&nbsp;<span></span>&nbsp;
+          Sony&nbsp;<span></span>&nbsp;
+          PS5
+        </div>
+      </div>
     </>
   )
 }
